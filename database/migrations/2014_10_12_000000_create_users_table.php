bbf845dc2101360265be1dbb089241f2d62fe05d
Santino Isgrò <santiisgro96@gmail.com>
2020-08-17 16:32:35 +0200

    Aggiunta immagine profilo di default

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,7 +19,7 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table-> string('google_id')->nullable();
             $table->timestamp('email_verified_at')->nullable();
-            $table->text('avatar')->nullable();
+            $table->text('avatar')->default("https://i.ibb.co/vVNFX1j/user.png");
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();

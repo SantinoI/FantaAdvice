274e7c26744f13b3a86950bade21e4ae516ec885
Santino Isgrò <santiisgro96@gmail.com>
2020-08-18 13:15:27 +0200

    Aggiunta la tabella Team collegata a ciascun utente e quindi implementata la sezione "yourTeam"

--- app/Http/Controllers/PollController.php
+++ app/Http/Controllers/PollController.php
@@ -8,13 +8,15 @@ class PollController extends Controller{
 
     public function store(Request $request){
 
+        $player1_check = request()->validate(['player1_selected' => 'required|min:1']);
+        
+        $player2_check = request()->validate(['player2_selected' => 'required|min:1']);
+
         Poll::create([
             'user_id' => auth()->id(),
-            'player1' =>  $request->input('player1_selected'),
-            'player2' =>  $request->input('player2_selected')
+            'player1' =>  $player1_check['player1_selected'],
+            'player2' =>  $player2_check['player2_selected']
         ]);
-
-        
 
         return redirect('/home');
     }
--- resources/views/yourTeam.blade.php
+++ resources/views/yourTeam.blade.php
@@ -1,11 +1,14 @@
 <h3 class = "font-bold text-lg mb-4 my-2 block">Ultimi voti del tuo team</h3>
 
 <ul>
-    @foreach(range(1,25) as $index)
+    @foreach($players as $player)
     <li class="my-5 ">
-        <div class="flex item-center ">
-            <img src="/" alt="" class="rounded-full mr-4">
-            Santino Isgrò
+        <div class="items-center">
+            <img src="{{$player->img}}" alt="" class="rounded-full ml-2 mr-4 ">
+        <p class="font-bold ml-2">{{$player->name}}</p>
+        <p class=" ml-2"> Juventus</p>
+        <p class="font-bold ml-2"> {{$player->fantamedia}}</p>
+        <hr>
         </div>
     </li>
     @endforeach

8013c7dc2dbfc1ffb49ea7670defed7cd6731782
Santino Isgrò <santiisgro96@gmail.com>
2020-08-19 09:50:15 +0200

    Aggiunto Messaggio nel caso in cui la lista del Team è vuota

--- resources/views/yourTeam.blade.php
+++ resources/views/yourTeam.blade.php
@@ -1,7 +1,7 @@
 <h3 class = "font-bold text-lg mb-4 my-2 block">Ultimi voti del tuo team</h3>
 
 <ul>
-    @foreach($players as $player)
+    @forelse($players as $player)
     <li class="my-5 ">
         <div class="items-center">
             <img src="{{$player->img}}" alt="" class="rounded-full ml-2 mr-4 ">
@@ -11,5 +11,8 @@
         <hr>
         </div>
     </li>
-    @endforeach
+    @empty
+        <p class="p-4">Please insert your Team</p>
+
+    @endforelse
 </ul>

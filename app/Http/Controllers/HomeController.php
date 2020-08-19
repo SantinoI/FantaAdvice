8045d4d5cc6b5560338722561a375e60b586c622
Santino Isgrò <santiisgro96@gmail.com>
2020-08-19 19:21:41 +0200

    Risolti conflitti git

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,10 +3,7 @@
 namespace App\Http\Controllers;
 use App\Poll;
 use App\Player;
-<<<<<<< HEAD
 use App\User;
-=======
->>>>>>> master
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -19,11 +16,7 @@ class HomeController extends Controller
 
     public function index()
     {
-<<<<<<< HEAD
         $players = User::find(auth()->user()->id)->player;
-=======
-        $players = Player::All();
->>>>>>> master
         return view('home',[
             'polls' => auth()->user()->timeline(),
             'players' => $players,
--- app/User.php
+++ app/User.php
@@ -44,11 +44,8 @@ class User extends Authenticatable
     public function player(){
         return $this->hasMany(Player::class);
     }
-<<<<<<< HEAD
 
     public function players(){
         return $this->hasOne('App\Player');
     }
-=======
->>>>>>> master
 }
